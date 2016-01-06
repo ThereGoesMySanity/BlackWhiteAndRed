fb06ea49720f56424cb1467dc6e28a3f68319893
Will Kennedy <willmlb@sbcglobal.net>
2016-01-06 11:00:09 -0500

    Fixed that one annoying easter egg

--- BlackWhiteAndRed/src/BlackWhiteAndRed.java
+++ BlackWhiteAndRed/src/BlackWhiteAndRed.java
@@ -39,6 +39,11 @@ public class BlackWhiteAndRed {
 	private JLabel lblMoney;
 	private JLabel lblDie;
 	private JButton playButton;
+
+	public BlackWhiteAndRed(){
+		this(Integer.parseInt(JOptionPane.showInputDialog("How much money do you want to start with?")));
+	}
+
 	public BlackWhiteAndRed(int balance){
 		money = highestBalance = balance;
 		die = new Die();
@@ -46,12 +51,12 @@ public class BlackWhiteAndRed {
 		key = new Scanner(System.in);
 		games = wins = 0;
 		stage = 0;
-		
-		
-		
+
+
+
 		frame = new JFrame();
 		frame.getContentPane().setLayout(null);
-		
+
 		instructions = new JTextPane();
 		instructions.setBackground(SystemColor.window);
 		instructions.setBounds(0, 0, 450, 90);
@@ -65,7 +70,7 @@ public class BlackWhiteAndRed {
 				+ "If that matches, you win $40.\n"
 				+ "Otherwise, you lose.");
 		frame.getContentPane().add(instructions);
-		
+
 		dieImg = new JLabel();
 		dieImg.addMouseListener(new MouseAdapter() {  //This makes it so if you click the die picture, it rolls it
 			public void mousePressed(MouseEvent e) {
@@ -77,7 +82,7 @@ public class BlackWhiteAndRed {
 		dieImg.setBounds(0, 130, 150, 120);
 		frame.getContentPane().add(dieImg);
 		dieImg.setIcon(getimg("/1-dice-clipart-4cbRaxecg.jpeg", dieImg));
-		
+
 		card1Img = new JLabel();
 		card1Img.addMouseListener(new MouseAdapter() {  //This makes it so if you click the card, it flips
 			public void mousePressed(MouseEvent e) {
@@ -89,7 +94,7 @@ public class BlackWhiteAndRed {
 		card1Img.setBounds(165, 120, 120, 170);
 		frame.getContentPane().add(card1Img);
 		card1Img.setIcon(getimg("/back-red_1024x1024.png", card1Img));
-		
+
 		card2Img = new JLabel();
 		card2Img.addMouseListener(new MouseAdapter() {  //This makes it... you get the idea
 			public void mousePressed(MouseEvent e) {
@@ -101,7 +106,7 @@ public class BlackWhiteAndRed {
 		card2Img.setBounds(315, 120, 120, 170);
 		frame.getContentPane().add(card2Img);
 		card2Img.setIcon(getimg("/back-red_1024x1024.png", card2Img));
-		
+
 		playButton = new JButton("Play");
 		/*
 		 * This is one of the important parts in the middle of all the nonsense!
@@ -115,67 +120,58 @@ public class BlackWhiteAndRed {
 		});
 		playButton.setBounds(150, 90, 150, 24);
 		frame.getContentPane().add(playButton);
-		
-		lblMoney = new JLabel("Money: $10");
+
+		lblMoney = new JLabel("Money: $"+money);
 		lblMoney.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMoney.setBounds(0, 90, 150, 24);
 		frame.getContentPane().add(lblMoney);
-		
+
 		lblWin = new JLabel("Win %:");
-		lblWin.addMouseListener(new MouseAdapter() {   //Easter egg: if you click on the Win % line,
-			public void mousePressed(MouseEvent e) {   //it plays automatically 500 times!
-				for(int i = 0; i < 500; i++){
-					gameSwitch(stage);
-				}
-			}
-		});
 		lblWin.setHorizontalAlignment(SwingConstants.CENTER);
 		lblWin.setBounds(300, 90, 150, 24);
 		frame.getContentPane().add(lblWin);
-		
+
 		lblDie = new JLabel("");
 		lblDie.setForeground(Color.RED);
 		lblDie.setFont(new Font("Dialog", Font.BOLD, 20));
 		lblDie.setHorizontalAlignment(SwingConstants.CENTER);
 		lblDie.setBounds(0, 250, 150, 20);
 		frame.getContentPane().add(lblDie);
-		
-		
+
+
 		frame.setSize(450, 320);
 		frame.setResizable(false);
 		frame.setVisible(true);
 	}
-	
+
 	//Runs when the game ends, lose or win.
 	public void update(){
 		updateStats();
 		stage = -2;
 		playButton.setText("Play Again");
 	}
-	
+
 	//Runs when you lose
 	public void lose(){
 		JOptionPane.showMessageDialog(frame,
-			    "You are a loser.",
-			    "You Lose",
-			    JOptionPane.PLAIN_MESSAGE);
+
+				"You are a loser.",
+				"You Lose",
+				JOptionPane.PLAIN_MESSAGE);
 		update();
-		
-		
 	}
-	
+
 	//Runs when you win
 	public void win(){
 		wins++;
 		money += 40;
 		JOptionPane.showMessageDialog(frame,
-			    "You are a winner.",
-			    "You Win",
-			    JOptionPane.PLAIN_MESSAGE);
+				"You are a winner.",
+				"You Win",
+				JOptionPane.PLAIN_MESSAGE);
 		update();
-		
 	}
-	
+
 	//Runs game
 	public void stage1(){
 		games++;
@@ -218,7 +214,7 @@ public class BlackWhiteAndRed {
 		}
 		win();
 	}
-	
+
 	//Updates various statistics
 	public void updateStats(){
 		lblMoney.setText("Money: $"+money);
@@ -226,16 +222,16 @@ public class BlackWhiteAndRed {
 		if(money>highestBalance)highestBalance=money;
 		if(money==0){
 			String s = (String)JOptionPane.showInputDialog(frame,
-				    "You're out of money.\nAdd more:",
-				    "You Lose",
-				    JOptionPane.PLAIN_MESSAGE);
+					"You're out of money.\nAdd more:",
+					"You Lose",
+					JOptionPane.PLAIN_MESSAGE);
 			Pattern p = Pattern.compile("\\$?(\\d+)");
 			Matcher regex = p.matcher(s);
 			while(!regex.matches()){
 				String s2 = (String)JOptionPane.showInputDialog(frame,
-					    "That's not an amount of money...",
-					    "You Lose",
-					    JOptionPane.PLAIN_MESSAGE);
+						"That's not an amount of money...",
+						"You Lose",
+						JOptionPane.PLAIN_MESSAGE);
 				regex = p.matcher(s2);
 			}
 			money += Integer.parseInt(regex.group(1));
@@ -243,28 +239,28 @@ public class BlackWhiteAndRed {
 			if(money == 0)quit();
 		}
 	}
-	
+
 	//Handles exiting the game
 	public void quit(){
 		key.close();
 		System.exit(0);
 	}
-	
-	
+
+
 	//Convert files into images for use in the JLabels
 	private ImageIcon getimg(String image, JLabel label){
 		BufferedImage img = null;
-		
+
 		try {
-		    img = ImageIO.read(getClass().getResource(image));
+			img = ImageIO.read(getClass().getResource(image));
 		} catch (IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
 		return new ImageIcon(img.getScaledInstance(label.getWidth(), label.getHeight(),
-        Image.SCALE_SMOOTH));
-		
+				Image.SCALE_SMOOTH));
+
 	}
-	
+
 	//This moves through and runs the game
 	private void gameSwitch(int i){
 		switch(i){
@@ -287,7 +283,7 @@ public class BlackWhiteAndRed {
 			break;
 		case 3:
 			stage3();
-			
+
 			break;
 		}
 		stage++;
